1b7ac7c6318f0532c4aedf480e21b99aaf25ef89
IValerieI <val.samarchenko@gmail.com>
2023-03-08 14:42:47 +0300

    Added DbContext

---
+++ Notes.Backend/Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+/// Interface which implemented by NotesDbContext in Notes.Persistence
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
---
+++ Notes.Backend/Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+/// Settings for Note (entity), just not to write that all 
+/// in method OnModelCreating()
+
+namespace Notes.Persistence.EntityTypeConfigurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(n => n.Id);
+            builder.HasIndex(n => n.Id).IsUnique();
+
+            builder.Property(n => n.Title).HasMaxLength(250);
+            builder.Property(n => n.Title).IsRequired();
+
+            builder.Property(n => n.Text).HasMaxLength(3000);
+
+        }
+    }
+}
